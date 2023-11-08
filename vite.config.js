48cc5f64745ac03036de6085af6292955346f109
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2023-11-08 11:10:17 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: '127.0.0.1'
+        }
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+            ],
+        }),
+    ],
+});
